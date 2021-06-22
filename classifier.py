fff4e10691649861ebbef023e4e79cafdea1f2f8
Akumar144 <akumar144@student.fuhsd.org>
2021-06-22 12:49:44 -0700

    Updated files

--- classifier.py
+++ classifier.py
@@ -47,21 +47,10 @@ n_test = X_test.shape[0]  # Number of testing examples
 n_validation = X_valid.shape[0]  # Number of validation examples
 n_classes = len(np.unique(y_train))  # Number of classes in dataset
 
-# reformat data
-# y_train = np_utils.to_categorical(y_train, n_classes)
-# y_test = np_utils.to_categorical(y_test, n_classes)
-# y_valid = np_utils.to_categorical(y_valid, n_classes)
-
-# Model parameters
-x = tf.placeholder(tf.float32, (None, 32, 32, 1))  # placeholder for batch of 32 by 32 greyscaled images
-y = tf.placeholder(tf.int32, (None))  # placeholder for labels
-
-keep_prob = tf.placeholder(tf.float32)  # For fully-connected layers
-keep_prob_conv = tf.placeholder(tf.float32)  # For convolutional layers
-
-EPOCHS = 10
+EPOCHS = 15
 BATCH_SIZE = 64
 DIR = 'Saved_Models'
+SHOW_DATASET = False
 
 
 def list_images(dataset, dataset_y, ylabel="", cmap=None):
@@ -103,6 +92,17 @@ def histogram_plot(dataset, label):
     plt.show()
 
 
+def visualizeDataset():
+    # Plotting sample examples, before pre-processing
+    # list_images(X_train, y_train, "Training example")
+    # list_images(X_test, y_test, "Testing example")
+    # list_images(X_valid, y_valid, "Validation example")
+    # Show frequency of each label
+    histogram_plot(y_train, "Training examples")
+    histogram_plot(y_test, "Testing examples")
+    histogram_plot(y_valid, "Validation examples")
+
+
 def gray_scale(image):
     """
     Convert images to gray scale.
@@ -136,150 +136,21 @@ def image_normalize(image):
 def preprocess(data):  # step 3
     # Sample images after greyscaling
     gray_images = list(map(gray_scale, data))
-    list_images(gray_images, y_train, "Gray Scale image", "gray")
+    # list_images(gray_images, y_train, "Gray Scale image", "gray")
 
     # Equalize images using skimage to improve contrast
     # Sample images after Local Histogram Equalization
     equalized_images = list(map(local_histo_equalize, gray_images))
-    list_images(equalized_images, y_train, "Equalized Image", "gray")
+    # list_images(equalized_images, y_train, "Equalized Image", "gray")
 
     # Normalize images
     n_training = data.shape
     normalized_images = np.zeros((n_training[0], n_training[1], n_training[2]))
     for i, img in enumerate(equalized_images):
         normalized_images[i] = image_normalize(img)
-    list_images(normalized_images, y_train, "Normalized Image", "gray")
+    # list_images(normalized_images, y_train, "Normalized Image", "gray")
     normalized_images = normalized_images[..., None]
     return normalized_images
-
-
-class VGGnet:
-    # todo: change to keras
-    def __init__(self, n_out=43, mu=0, sigma=0.1, learning_rate=0.001):
-        # Hyperparameters
-        self.mu = mu
-        self.sigma = sigma
-
-        # Layer 1 (Convolutional): Input = 32x32x1. Output = 32x32x32.
-        self.conv1_W = tf.Variable(tf.truncated_normal(shape=(3, 3, 1, 32), mean=self.mu, stddev=self.sigma))
-        self.conv1_b = tf.Variable(tf.zeros(32))
-        self.conv1 = tf.nn.conv2d(x, self.conv1_W, strides=[1, 1, 1, 1], padding='SAME') + self.conv1_b
-
-        # ReLu Activation.
-        self.conv1 = tf.nn.relu(self.conv1)
-
-        # Layer 2 (Convolutional): Input = 32x32x32. Output = 32x32x32.
-        self.conv2_W = tf.Variable(tf.truncated_normal(shape=(3, 3, 32, 32), mean=self.mu, stddev=self.sigma))
-        self.conv2_b = tf.Variable(tf.zeros(32))
-        self.conv2 = tf.nn.conv2d(self.conv1, self.conv2_W, strides=[1, 1, 1, 1], padding='SAME') + self.conv2_b
-
-        # ReLu Activation.
-        self.conv2 = tf.nn.relu(self.conv2)
-
-        # Layer 3 (Pooling): Input = 32x32x32. Output = 16x16x32.
-        self.conv2 = tf.nn.max_pool(self.conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-        self.conv2 = tf.nn.dropout(self.conv2, keep_prob_conv)
-
-        # Layer 4 (Convolutional): Input = 16x16x32. Output = 16x16x64.
-        self.conv3_W = tf.Variable(tf.truncated_normal(shape=(3, 3, 32, 64), mean=self.mu, stddev=self.sigma))
-        self.conv3_b = tf.Variable(tf.zeros(64))
-        self.conv3 = tf.nn.conv2d(self.conv2, self.conv3_W, strides=[1, 1, 1, 1], padding='SAME') + self.conv3_b
-
-        # ReLu Activation.
-        self.conv3 = tf.nn.relu(self.conv3)
-
-        # Layer 5 (Convolutional): Input = 16x16x64. Output = 16x16x64.
-        self.conv4_W = tf.Variable(tf.truncated_normal(shape=(3, 3, 64, 64), mean=self.mu, stddev=self.sigma))
-        self.conv4_b = tf.Variable(tf.zeros(64))
-        self.conv4 = tf.nn.conv2d(self.conv3, self.conv4_W, strides=[1, 1, 1, 1], padding='SAME') + self.conv4_b
-
-        # ReLu Activation.
-        self.conv4 = tf.nn.relu(self.conv4)
-
-        # Layer 6 (Pooling): Input = 16x16x64. Output = 8x8x64.
-        self.conv4 = tf.nn.max_pool(self.conv4, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-        self.conv4 = tf.nn.dropout(self.conv4, keep_prob_conv)  # dropout
-
-        # Layer 7 (Convolutional): Input = 8x8x64. Output = 8x8x128.
-        self.conv5_W = tf.Variable(tf.truncated_normal(shape=(3, 3, 64, 128), mean=self.mu, stddev=self.sigma))
-        self.conv5_b = tf.Variable(tf.zeros(128))
-        self.conv5 = tf.nn.conv2d(self.conv4, self.conv5_W, strides=[1, 1, 1, 1], padding='SAME') + self.conv5_b
-
-        # ReLu Activation.
-        self.conv5 = tf.nn.relu(self.conv5)
-
-        # Layer 8 (Convolutional): Input = 8x8x128. Output = 8x8x128.
-        self.conv6_W = tf.Variable(tf.truncated_normal(shape=(3, 3, 128, 128), mean=self.mu, stddev=self.sigma))
-        self.conv6_b = tf.Variable(tf.zeros(128))
-        self.conv6 = tf.nn.conv2d(self.conv5, self.conv6_W, strides=[1, 1, 1, 1], padding='SAME') + self.conv6_b
-
-        # ReLu Activation.
-        self.conv6 = tf.nn.relu(self.conv6)
-
-        # Layer 9 (Pooling): Input = 8x8x128. Output = 4x4x128.
-        self.conv6 = tf.nn.max_pool(self.conv6, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-        self.conv6 = tf.nn.dropout(self.conv6, keep_prob_conv)  # dropout
-
-        # Flatten. Input = 4x4x128. Output = 2048.
-        self.fc0 = flatten(self.conv6)
-
-        # Layer 10 (Fully Connected): Input = 2048. Output = 128.
-        self.fc1_W = tf.Variable(tf.truncated_normal(shape=(2048, 128), mean=self.mu, stddev=self.sigma))
-        self.fc1_b = tf.Variable(tf.zeros(128))
-        self.fc1 = tf.matmul(self.fc0, self.fc1_W) + self.fc1_b
-
-        # ReLu Activation.
-        self.fc1 = tf.nn.relu(self.fc1)
-        self.fc1 = tf.nn.dropout(self.fc1, keep_prob)  # dropout
-
-        # Layer 11 (Fully Connected): Input = 128. Output = 128.
-        self.fc2_W = tf.Variable(tf.truncated_normal(shape=(128, 128), mean=self.mu, stddev=self.sigma))
-        self.fc2_b = tf.Variable(tf.zeros(128))
-        self.fc2 = tf.matmul(self.fc1, self.fc2_W) + self.fc2_b
-
-        # ReLu Activation.
-        self.fc2 = tf.nn.relu(self.fc2)
-        self.fc2 = tf.nn.dropout(self.fc2, keep_prob)  # dropout
-
-        # Layer 12 (Fully Connected): Input = 128. Output = n_out.
-        self.fc3_W = tf.Variable(tf.truncated_normal(shape=(128, n_out), mean=self.mu, stddev=self.sigma))
-        self.fc3_b = tf.Variable(tf.zeros(n_out))
-        self.logits = tf.matmul(self.fc2, self.fc3_W) + self.fc3_b
-
-        # Training operation
-        self.one_hot_y = tf.one_hot(y, n_out)
-        self.cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.one_hot_y)
-        self.loss_operation = tf.reduce_mean(self.cross_entropy)
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-        self.training_operation = self.optimizer.minimize(self.loss_operation)
-
-        # Accuracy operation
-        self.correct_prediction = tf.equal(tf.argmax(self.logits, 1), tf.argmax(self.one_hot_y, 1))
-        self.accuracy_operation = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
-
-        # Saving all variables
-        self.saver = tf.train.Saver()
-
-    def y_predict(self, X_data, BATCH_SIZE=64):
-        num_examples = len(X_data)
-        y_pred = np.zeros(num_examples, dtype=np.int32)
-        sess = tf.get_default_session()
-        for offset in range(0, num_examples, BATCH_SIZE):
-            batch_x = X_data[offset:offset + BATCH_SIZE]
-            y_pred[offset:offset + BATCH_SIZE] = sess.run(tf.argmax(self.logits, 1),
-                                                          feed_dict={x: batch_x, keep_prob: 1, keep_prob_conv: 1})
-        return y_pred
-
-    def evaluate(self, X_data, y_data, BATCH_SIZE=64):
-        num_examples = len(X_data)
-        total_accuracy = 0
-        sess = tf.get_default_session()
-        for offset in range(0, num_examples, BATCH_SIZE):
-            batch_x, batch_y = X_data[offset:offset + BATCH_SIZE], y_data[offset:offset + BATCH_SIZE]
-            accuracy = sess.run(self.accuracy_operation,
-                                feed_dict={x: batch_x, y: batch_y, keep_prob: 1.0, keep_prob_conv: 1.0})
-            total_accuracy += (accuracy * len(batch_x))
-        return total_accuracy / num_examples
 
 
 class Model:
@@ -323,174 +194,57 @@ class Model:
         predictions = self.model.predict_classes(X_data)
         return predictions
 
+    def y_predict_prob(self, X_data):
+        prob = self.model.predict(X_data)
+        return prob
+
     def evaluate(self, X_data, y_data):
         score = self.model.evaluate(X_data, y_data)
         return score
 
 
-def runModel(normalized_images):
-    global X_train, y_train
-    # todo: change to keras
-    VGGNet_Model = VGGnet(n_out=n_classes)
-    model_name = "VGGNet"
-
-    # Validation set preprocessing
-    X_valid_preprocessed = preprocess(X_valid)
-    one_hot_y_valid = tf.one_hot(y_valid, 43)
-
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        num_examples = len(y_train)
-        print("Training...")
-        print()
-        for i in range(EPOCHS):
-            normalized_images, y_train = shuffle(normalized_images, y_train)
-            for offset in range(0, num_examples, BATCH_SIZE):
-                end = offset + BATCH_SIZE
-                batch_x, batch_y = normalized_images[offset:end], y_train[offset:end]
-                sess.run(VGGNet_Model.training_operation,
-                         feed_dict={x: batch_x, y: batch_y, keep_prob: 0.5, keep_prob_conv: 0.7})
-
-            validation_accuracy = VGGNet_Model.evaluate(X_valid_preprocessed, y_valid)
-            print("EPOCH {} : Validation Accuracy = {:.3f}%".format(i + 1, (validation_accuracy * 100)))
-        VGGNet_Model.saver.save(sess, os.path.join(DIR, model_name))
-        print("Model saved")
-    return VGGNet_Model
-
-
 def trainModelKeras(normalized_images):
     global X_train, y_train
-    # todo: change to keras
     kerasModel = Model(n_out=n_classes)
     model_name = "Keras"
 
     # Validation set preprocessing
     X_valid_preprocessed = preprocess(X_valid)
-    # one_hot_y_valid = tf.one_hot(y_valid, 43)
     y_train_onehot = np_utils.to_categorical(y_train, n_classes)
     y_valid_onehot = np_utils.to_categorical(y_valid, n_classes)
     kerasModel.model.fit(normalized_images, y_train_onehot, epochs=EPOCHS, batch_size=BATCH_SIZE,
-                         validation_data=(X_valid_preprocessed, y_valid_onehot))
+                         validation_data=(X_valid_preprocessed, y_valid_onehot), verbose=0)
     return kerasModel
 
 
-def y_predict_model(Input_data, VGGNet_Model, top_k=5):
+def y_predict_model(Input_data, model, top_k=5):
     """
     Generates the predictions of the model over the input data, and outputs the top softmax probabilities.
         Parameters:
             X_data: Input data.
             top_k (Default = 5): The number of top softmax probabilities to be generated.
     """
-    num_examples = len(Input_data)
-    y_pred = np.zeros((num_examples, top_k), dtype=np.int32)
-    y_prob = np.zeros((num_examples, top_k))
-    with tf.Session() as sess:
-        VGGNet_Model.saver.restore(sess, os.path.join(DIR, "VGGNet"))
-        y_prob, y_pred = sess.run(tf.nn.top_k(tf.nn.softmax(VGGNet_Model.logits), k=top_k),
-                                  feed_dict={x: Input_data, keep_prob: 1, keep_prob_conv: 1})
-    return y_prob, y_pred
-
-
-def main_backup():
-    global X_train, y_train
-    # Plotting sample examples, before pre-processing
-    list_images(X_train, y_train, "Training example")
-    list_images(X_test, y_test, "Testing example")
-    list_images(X_valid, y_valid, "Validation example")
-    # Show frequency of each label
-    histogram_plot(y_train, "Training examples")
-    histogram_plot(y_test, "Testing examples")
-    histogram_plot(y_valid, "Validation examples")
-
-    # Step 3, preprocessing
-
-    # Randomize dataset to improve training, using sklearn
-    X_train, y_train = shuffle(X_train, y_train)
-    X_train_normalized = preprocess(X_train)
-    #  Step 4, training
-    VGGNet_Model = trainModelKeras(X_train_normalized)
-
-    # Step 5, testing
-    # todo: change to keras
-    X_test_preprocessed = preprocess(X_test)
-    with tf.Session() as sess:
-        VGGNet_Model.saver.restore(sess, os.path.join(DIR, "VGGNet"))
-        y_pred = VGGNet_Model.y_predict(X_test_preprocessed)
-        test_accuracy = sum(y_test == y_pred) / len(y_test)
-        print("Test Accuracy = {:.1f}%".format(test_accuracy * 100))
-    # Show model results, and failures
-    cm = confusion_matrix(y_test, y_pred)
-    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    cm = np.log(.0001 + cm)
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
-    plt.title('Log of normalized Confusion Matrix')
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.show()
-
-    # Step 6, testing new images(outside dataset)
-    new_test_images = []
-    path = './traffic-signs-data/new_test_images/'
-    for image in os.listdir(path):
-        img = cv2.imread(path + image)
-        img = cv2.resize(img, (32, 32))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        new_test_images.append(img)
-    new_IDs = [13, 3, 14, 27, 17]
-    print("Number of new testing examples: ", len(new_test_images))
-
-    plt.figure(figsize=(15, 16))
-    for i in range(len(new_test_images)):
-        plt.subplot(2, 5, i + 1)
-        plt.imshow(new_test_images[i])
-        plt.xlabel(signs[new_IDs[i]])
-        plt.ylabel("New testing image")
-        plt.xticks([])
-        plt.yticks([])
-    plt.tight_layout(pad=0, h_pad=0, w_pad=0)
-    plt.show()
-
-    # New test data preprocessing
-    new_test_images_preprocessed = preprocess(np.asarray(new_test_images))
-
-    # get predictions
-    y_prob, y_pred = y_predict_model(new_test_images_preprocessed, VGGNet_Model)
-
-    # generate summary of results
-    test_accuracy = 0
-    for i in enumerate(new_test_images_preprocessed):
-        accu = new_IDs[i[0]] == np.asarray(y_pred[i[0]])[0]
-        if accu == True:
-            test_accuracy += 0.2
-    print("New Images Test Accuracy = {:.1f}%".format(test_accuracy * 100))
-
-    plt.figure(figsize=(15, 16))
-    new_test_images_len = len(new_test_images_preprocessed)
-    for i in range(new_test_images_len):
-        plt.subplot(new_test_images_len, 2, 2 * i + 1)
-        plt.imshow(new_test_images[i])
-        plt.title(signs[y_pred[i][0]])
-        plt.axis('off')
-        plt.subplot(new_test_images_len, 2, 2 * i + 2)
-        plt.barh(np.arange(1, 6, 1), y_prob[i, :])
-        labels = [signs[j] for j in y_pred[i]]
-        plt.yticks(np.arange(1, 6, 1), labels)
-    plt.show()
+    probs = model.y_predict_prob(Input_data)
+    y_probs = []
+    y_preds = []
+    for prob in probs:
+        y_pred = []
+        y_prob = []
+        for i in range(top_k):
+            y_pred.append(np.argmax(prob))
+            y_prob.append(prob[y_pred[-1]])
+            prob[y_pred[-1]] = 0
+        y_probs.append(y_prob)
+        y_preds.append(y_pred)
+    return np.array(y_probs), np.array(y_preds)
 
 
 def main():
     global X_train, y_train
-    # Plotting sample examples, before pre-processing
-    list_images(X_train, y_train, "Training example")
-    list_images(X_test, y_test, "Testing example")
-    list_images(X_valid, y_valid, "Validation example")
-    # Show frequency of each label
-    histogram_plot(y_train, "Training examples")
-    histogram_plot(y_test, "Testing examples")
-    histogram_plot(y_valid, "Validation examples")
 
     # Step 3, preprocessing
-
+    if SHOW_DATASET:
+        visualizeDataset()
     # Randomize dataset to improve training, using sklearn
     X_train, y_train = shuffle(X_train, y_train)
     X_train_normalized = preprocess(X_train)
@@ -541,8 +295,7 @@ def main():
     new_test_images_preprocessed = preprocess(np.asarray(new_test_images))
 
     # get predictions
-    y_prob, y_pred = y_predict_model(new_test_images_preprocessed, VGGNet_Model)
-
+    y_prob, y_pred = y_predict_model(new_test_images_preprocessed, kerasModel)
     # generate summary of results
     test_accuracy = 0
     for i in enumerate(new_test_images_preprocessed):
